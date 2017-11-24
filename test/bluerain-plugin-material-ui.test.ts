464326139417aa6603fa8c0aa6869aa6afa8926e
Umair Farooq <umair.farooq@blueeast.com>
2017-11-24 16:37:03 +0500

    Tests added

--- test/bluerain-plugin-material-ui.test.ts
+++ test/bluerain-plugin-material-ui.test.ts
@@ -1,4 +1,5 @@
 import BR, { Plugin } from '@blueeast/bluerain-os';
+import IntlPlugin from '@blueeast/bluerain-plugin-intl';
 // tslint:disable-next-line:no-implicit-dependencies
 import React from 'react';
 import MaterialUiPlugin from '../src/index';
@@ -29,5 +30,18 @@ describe('material-ui Plugin Test', () => {
 		BR.boot();
 
 		expect(BR.Configs.get('theme')).toEqual(theme);
+	});
+	it('Should boot with intl', () => {
+		BR.boot({ plugins: [IntlPlugin, MaterialUiPlugin] });
+		expect(BR.Plugins.get('intl')).toBeDefined();
+	});
+	it('should boot with custom config', () => {
+		BR.boot({
+			plugins: [MaterialUiPlugin],
+			config: { plugins: { 'material-ui': 'customConfgi' } }
+		});
+		expect(BR.Configs.ConfigsTable.plugins['material-ui']).toEqual(
+			'customConfgi'
+		);
 	});
 });
