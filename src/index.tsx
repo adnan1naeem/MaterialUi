87a8c6d732d18e8885c4580425c008d8b6db7e42
ghazaltaimur <ghazal.taimur@blueeast.com>
2017-12-29 23:54:17 +0500

    remove lint issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import * as React from 'react';
 import { Plugin ,bluerain} from '@blueeast/bluerain-os';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
