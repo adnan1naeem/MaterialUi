daa2043be5ea694b590807b31a47502f4a9a7573
waheed <waheed.akhtar@blueeast.com>
2017-12-29 12:00:00 +0500

    test folder added

---
+++ src/components/Test/component/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import { withStyles } from 'material-ui/styles';
+
+const Test=()=>{
+
+return (
+    <List>
+        <ListItem button>
+            <ListItemText primary="Testing" />
+        </ListItem></List>
+);
+};
+export default Test;
