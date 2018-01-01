d2a9b316eea013478138cad34aff3df2a472191b
Ghulam Hussain <ghulam.hussain@blueeast.com>
2018-01-01 18:12:26 +0500

    added docs

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -4,10 +4,19 @@ import BadgeUi from 'material-ui/Badge';
 import { FormattedMessage } from 'react-intl';
 import { IBadge } from '@blueeast/bluerain-ui-interfaces/Components/Badge';
 
+/**
+ * The universal props of Badge Component
+ * @param {Object} props.style Get the style object
+ * @param {ReactNode} props.children Content of the Badge
+ * The universal props of Badge Component
+ * @param {enum} props.color The enum color of Badge.
+ * @param {ReactNode} props.className the className for styles.
+ */
+
 export interface IBadgeProps {
-    color?: any;
+    color?: 'default' | 'primary' | 'accent';
     children?: Node;
-    className?: any;
+    className?: string;
 }
 const Badge = (props: IBadge & IBadgeProps) => {
     return (<BadgeUi badgeContent={props.badgeContent} classes={{...props.style}} {...props} color={props.color}>{props.children}</BadgeUi>);
