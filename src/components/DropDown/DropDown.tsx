f3f0292c098bdae37f70d5a31e0725952b312d0c
waheed <waheed.akhtar@blueeast.com>
2018-01-03 15:58:16 +0500

    DropDown Component finalized

---
+++ src/components/DropDown/DropDown.tsx
@@ -0,0 +1,39 @@
+import React,{ReactNode} from 'react';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
+import Input, { InputLabel } from 'material-ui/Input';
+
+import  {IPicker} from '@blueeast/bluerain-ui-interfaces/Components/Picker';
+/**
+ * The props of DropDown Component
+ * @param {boolean} props.autoWidth for Auto width.
+ * @param {Object} props.classes style object for DopDown style.
+ * @param {boolean} props.displayEmpty If true, the selected item is displayed even if its value is empty. You can only use it when the native property is false (default)..
+ * @param {ReactNode} props.input An Input element; does not have to be a material-ui specific Input.
+ * @param {Object} props.MenuProps Properties applied to the Menu element.
+ * @param {boolean} props.native If true, the component will be using a native select element.
+ * @param {Node} props.renderValue Render the selected value. You can only use it when the native property is false (default).
+ * @param {Node} props.value The input value, required for a controlled component.
+ * @param {boolean} props.disabled disables the selection.
+ */
+interface IMaterialuiDropDown {
+    autoWidth?:boolean,
+    classes?:object,
+    displayEmpty?:boolean,
+    input?:ReactNode,
+    MenuProps?:object,
+    input:ReactNode,
+    native?:boolean,
+    renderValue?:Function,
+    disabled?:boolean
+};
+ const DropDown =(props:IPicker & IMaterialuiDropDown) =>(
+            <Select
+                onChange={props.onValueChange}
+                value={props.selectedValue}
+                {...props}>
+                </Select>
+);
+
+const DropdownItem = (props:IPicker  & IMaterialuiDropDown) => (<MenuItem {...props} />);
+export { DropdownItem, DropDown };
--- src/components/DropDown/test/DropDown.stories.tsx
+++ src/components/DropDown/test/DropDown.stories.tsx
@@ -1,34 +1,85 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Paper from 'material-ui/Paper';
-import DropDown from '../component/DropDown';
+import Input, { InputLabel } from 'material-ui/Input';
+import {DropDown,DropdownItem} from '../DropDown';
 const stories = storiesOf('DropDown', module);
 
-const DropDownList = [
-    {
-        id:1,
-        label:'Home',
-        value:'home',
-        image:'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'
-     },
-    {
-        id:2,
-        label:'Work',
-        value:'work',
-        image:'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'
-    }, {
-        id:1,
-        label:'Outside',
-        value:'Outside',
-        image:'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'
+const onChange =(event) => {
+    const value=event.target.value;
+};
+const style={
+    root:{
+        padding:20
     },
-    {
-        id:2,
-        label:'Work',
-        value:'work',
-        image:'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'
-    }
-];
-stories.add('DropDown', () => (
-    <Paper style={{padding:20}}><DropDown onChange={()=>console.log('Button pressed')}  style={{width:400}}  value={'home'} items={DropDownList}></DropDown></Paper>
-));
+   dropdown:{
+       width:400
+   }
+};
+const multiItems=['Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',];
+stories.add('DropDown', () => {
+    return(
+    <Paper style={style.root}>
+        <DropDown onValueChange={onChange} value={'xyz'}  style={style.dropdown}>
+          <DropdownItem value="xyz">Item 1 </DropdownItem>
+            <DropdownItem value="abc">Item 2 </DropdownItem>
+            <DropdownItem value="abc">Item 3 </DropdownItem>
+            <DropdownItem value="abc">Item 4 </DropdownItem>
+            <DropdownItem value="abc">Item 5 </DropdownItem>
+        </DropDown>
+    </Paper>)}
+);
+stories.add('DropDown with autoWidth', () => {
+    return(
+        <Paper style={style.root}>
+            <DropDown onValueChange={onChange} value={'xyz'} autoWidth={true}  style={style.dropdown}>
+                <DropdownItem value="xyz">Item 1 </DropdownItem>
+                <DropdownItem value="abc">Item 2 </DropdownItem>
+                <DropdownItem value="abc">Item 3 </DropdownItem>
+                <DropdownItem value="abc">Item 4 </DropdownItem>
+                <DropdownItem value="abc">Item 5 </DropdownItem>
+            </DropDown>
+        </Paper>)}
+);stories.add('DropDown with Override styles', () => {
+    return(
+        <Paper style={style.root}>
+            <DropDown onValueChange={onChange} value={'xyz'} style={{width:600, backgroundColor:'green'}} itemStyle={{backgroundColor:'balck'}}>
+                <DropdownItem value="xyz">Item 1 </DropdownItem>
+                <DropdownItem value="abc">Item 2 </DropdownItem>
+                <DropdownItem value="abc">Item 3 </DropdownItem>
+                <DropdownItem value="abc">Item 4 </DropdownItem>
+                <DropdownItem value="abc">Item 5 </DropdownItem>
+            </DropDown>
+        </Paper>)}
+);stories.add('DropDown multiple select', () => {
+    return(
+        <Paper style={style.root}>
+            <DropDown onValueChange={onChange} value={multiItems} autoWidth={true} multiple style={style.dropdown} >
+                <DropdownItem value="xyz">Item 1 </DropdownItem>
+                <DropdownItem value="abc">Item 2 </DropdownItem>
+                <DropdownItem value="abc">Item 3 </DropdownItem>
+                <DropdownItem value="abc">Item 4 </DropdownItem>
+                <DropdownItem value="abc">Item 5 </DropdownItem>
+            </DropDown>
+        </Paper>)}
+);stories.add('DropDown with disabled', () => {
+    return(
+        <Paper style={style.root}>
+            <DropDown onValueChange={onChange} value={'xyz'}  input={<Input id="name-native-disabled" />} style={style.dropdown} displayEmpty>
+                <DropdownItem value="xyz">Item 1 </DropdownItem>
+                <DropdownItem disabled value="abc">Item 2</DropdownItem>
+                <DropdownItem disabled value="abc">Item 3 </DropdownItem>
+                <DropdownItem value="abc">Item 4 </DropdownItem>
+                <DropdownItem value="abc">Item 5 </DropdownItem>
+            </DropDown>
+        </Paper>)}
+);
