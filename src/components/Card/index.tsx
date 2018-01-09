9d0f8b6a7b5da84659d1988bbd0cbc2917dc3817
Ghazanfar <ghazanfar.ali@blueeast.com>
2018-01-09 12:30:00 +0500

    Resolved export default

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,5 @@
-import {default as  CardHeader } from './card/CardHeader.component';
-import {default as  CardMedia } from './card/CardMedia.component';
-import {default as  CardBody } from './card/CardBody.component';
+export {default as  CardHeader } from './card/CardHeader.component';
+export {default as  CardMedia } from './card/CardMedia.component';
+export {default as  CardBody } from './card/CardBody.component';
 export {default as  CardFooter } from './card/CardFooter.component';
 export { default } from './card/Card.component';
