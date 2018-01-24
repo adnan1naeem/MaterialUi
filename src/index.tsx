be1fba61049ae34aa41feb4cc785795afb057c42
amnajunaid <amna.junaid@blueeast.com>
2018-01-24 15:20:38 +0500

    fix: Conflicts resolved

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,11 @@ import { Plugin, bluerainType } from '@blueeast/bluerain-os';
 
 import TextInput from './components/textInput';
 import Chip from './components/chip';
-import GMap from './components/map';
 import Badge from './components/badge';
 import Avatar from './components/avatar';
 import Switch from './components/switch';
 import { DropDown, DropdownItem } from './components/DropDown/DropDown';
-import List, {ListItem, ListHeader, ListItemAvatar, ListItemIcon, ListItemRightButton, ListItemText} from './components/List';
+import List, { ListItem, ListHeader, ListItemAvatar, ListItemIcon, ListItemRightButton, ListItemText } from './components/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Button from './components/button';
 import IconComponent from './components/Icon';
@@ -38,39 +37,38 @@ import Breadcrumb from './components/Breadcrumb';
  */
 class MaterialUiPlugin extends Plugin {
 
-    static pluginName = 'MaterialUiPlugin';
-    static slug = 'material-ui';
+	static pluginName = 'MaterialUiPlugin';
+	static slug = 'material-ui';
 
-    static initialize(config:any = {}, ctx:bluerainType) {
+	static initialize(config:any = {}, ctx:bluerainType) {
 
-        let theme = ctx.Configs.get('theme');
-        if (!theme) {
-            theme = {};
-        }
+		let theme = ctx.Configs.get('theme');
+		if (!theme) {
+			theme = {};
+		}
 
-        ctx.Components.has('TextInput') ? ctx.Components.replace('TextInput', TextInput) : ctx.Components.set('TextInput', TextInput);
-        ctx.Components.set('Chip', Chip);
-        ctx.Components.set('GMap', GMap);
-        ctx.Components.set('Badge', Badge);
-        ctx.Components.set('Dropdown', DropDown);
-        ctx.Components.set('DropdownItem', DropdownItem);
-        ctx.Components.set('Avatar', Avatar);
-        ctx.Components.set('List', List);
-        ctx.Components.set('ListItem', ListItem);
-        ctx.Components.set('ListHeader', ListHeader);
-        ctx.Components.set('ListItemAvatar', ListItemAvatar);
-        ctx.Components.set('ListItemIcon', ListItemIcon);
-        ctx.Components.set('ListItemText', ListItemText);
-        ctx.Components.set('ListItemRightButton', ListItemRightButton);
-        ctx.Components.has('Button') ? ctx.Components.replace('Button', Button) : ctx.Components.set('Button', Button);
-        ctx.Components.set('Card', Card);
-        ctx.Components.set('CardHeader', CardHeader);
-        ctx.Components.set('CardBody', CardBody);
-        ctx.Components.set('CardMedia', CardMedia);
-        ctx.Components.set('CardFooter', CardFooter);
-        ctx.Components.set('Breadcrumb', Breadcrumb);
-        ctx.Components.set('SwitchToggle', Switch);
-        ctx.Components.set('Icon', IconComponent);
+		ctx.Components.has('TextInput') ? ctx.Components.replace('TextInput', TextInput) : ctx.Components.set('TextInput', TextInput);
+		ctx.Components.set('Chip', Chip);
+		ctx.Components.set('Badge', Badge);
+		ctx.Components.set('Dropdown', DropDown);
+		ctx.Components.set('DropdownItem', DropdownItem);
+		ctx.Components.set('Avatar', Avatar);
+		ctx.Components.set('List', List);
+		ctx.Components.set('ListItem', ListItem);
+		ctx.Components.set('ListHeader', ListHeader);
+		ctx.Components.set('ListItemAvatar', ListItemAvatar);
+		ctx.Components.set('ListItemIcon', ListItemIcon);
+		ctx.Components.set('ListItemText', ListItemText);
+		ctx.Components.set('ListItemRightButton', ListItemRightButton);
+		ctx.Components.has('Button') ? ctx.Components.replace('Button', Button) : ctx.Components.set('Button', Button);
+		ctx.Components.set('Card', Card);
+		ctx.Components.set('CardHeader', CardHeader);
+		ctx.Components.set('CardBody', CardBody);
+		ctx.Components.set('CardMedia', CardMedia);
+		ctx.Components.set('CardFooter', CardFooter);
+		ctx.Components.set('Breadcrumb', Breadcrumb);
+		ctx.Components.set('SwitchToggle', Switch);
+		ctx.Components.set('Icon', IconComponent);
 
         // Add Material UI Provider
 //         	ctx.Filters.add(
@@ -86,7 +84,7 @@ class MaterialUiPlugin extends Plugin {
 // 	return result;
 // }
 //         );
-}
+	}
 }
 
 export default MaterialUiPlugin;
