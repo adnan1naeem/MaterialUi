7347440eae1bc0adfb32a3e4d8101a88c628a257
Ghazanfar <ghazanfar.ali@blueeast.com>
2018-01-26 14:37:10 +0500

    fix(Breadcrumb tests passed):

--- test/Breadcrumb.test.tsx
+++ test/Breadcrumb.test.tsx
@@ -1,3 +1,5 @@
+
+import { MemoryRouter } from 'react-router-dom';
 import { create } from 'react-test-renderer';
 import Breadcrumb from '../src/components/Breadcrumb';
 import React from 'react';
@@ -7,7 +9,7 @@ test('Breadcrumb component label and data props', () => {
 	const data = [{ title:'CardView', path: 'https://www.facebook.com' }];
 	const label = 'Site';
 	const breadcrumb = create(
-		<Breadcrumb data={data} label={label}/>
+		<MemoryRouter><Breadcrumb data={data} label={label}/></MemoryRouter>
   ).toJSON();
 	expect(breadcrumb.children).toHaveLength(2);
 	expect(breadcrumb).toMatchSnapshot();
@@ -16,7 +18,7 @@ test('Breadcrumb component label, colorPrev and data props', () => {
 	const data = [{ title:'CardView', path: 'https://www.facebook.com' }];
 	const label = 'Site';
 	const breadcrumb = create(
-		<Breadcrumb data={data} label={label} colorPrev="purple" />
+		<MemoryRouter><Breadcrumb data={data} label={label} colorPrev="purple" /></MemoryRouter>
   ).toJSON();
 	expect(breadcrumb.children).toHaveLength(2);
 	expect(breadcrumb).toMatchSnapshot();
@@ -25,7 +27,7 @@ test('Breadcrumb component with no props, default style should be applied', () =
 
 	const defaultStyle = { color: 'black' };
 	const breadcrumb = create(
-		<Breadcrumb />
+		<MemoryRouter><Breadcrumb /></MemoryRouter>
   ).toJSON();
 	expect(breadcrumb.props.style).toEqual(defaultStyle);
 	expect(breadcrumb).toMatchSnapshot();
@@ -36,7 +38,7 @@ test('Breadcrumb component colorPrev prop', () => {
 	const defaultStyle = { color: 'black' };
 
 	const breadcrumb = create(
-		<Breadcrumb colorPrev="blue" />
+		<MemoryRouter><Breadcrumb colorPrev="blue" /></MemoryRouter>
   ).toJSON();
 	expect(breadcrumb.props.style).toEqual(defaultStyle);
 	expect(breadcrumb).toMatchSnapshot();
@@ -47,7 +49,7 @@ test('Breadcrumb component colorActive prop', () => {
 	const defaultStyle = { color: 'green' };
 
 	const breadcrumb = create(
-		<Breadcrumb colorPrev="blue" colorActive="green" />
+		<MemoryRouter><Breadcrumb colorPrev="blue" colorActive="green" /></MemoryRouter>
   ).toJSON();
 	expect(breadcrumb.props.style).toEqual(defaultStyle);
 	expect(breadcrumb).toMatchSnapshot();
