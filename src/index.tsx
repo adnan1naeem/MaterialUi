be726a22ef66000b71de53d8d9a82a57ddef3647
Babar Javaid <babar.javaid@blueeast.com>
2018-02-24 14:19:07 +0500

    fix(Lint Issues): resolve lint issues

--- src/index.tsx
+++ src/index.tsx
@@ -20,8 +20,8 @@ import { Button } from './components/Button';
  * @property {string} pluginName "MaterialUiPlugin"
  * @property {string} slug "material-ui"
  */
-class MaterialUiPlugin extends Plugin {
 
+class MaterialUiPlugin extends Plugin {
 	static pluginName = 'MaterialUiPlugin';
 	static slug = 'material-ui';
 
@@ -30,19 +30,15 @@ class MaterialUiPlugin extends Plugin {
 		Avatar,
 		Badge,
 		Button,
-
 		Card,
 		CardHeader,
 		CardBody,
 		CardMedia,
 		CardFooter,
-
 		Chip,
 		Divider,
-		
 		Dropdown,
 		DropdownItem,
-
 		List,
 		ListHeader,
 		ListItem,
@@ -50,36 +46,12 @@ class MaterialUiPlugin extends Plugin {
 		ListItemIcon,
 		ListItemRightButton,
 		ListItemText,
-
 		Switch,
-
 		FormControl,
 		FormGroup,
 		TextInput,
-
 		Breadcrumb, // Deprecated, remove in next semver major version
 	};
-
-// 		let theme = ctx.Configs.get('theme');
-// 		if (!theme) {
-// 			theme = {};
-// 		}
-
-//         // Add Material UI Provider
-// //         	ctx.Filters.add(
-// //           'bluerain.redux.app',
-// //       function AddMUI(App) {
-// //
-// // 	// const result = withMUI(App, theme, ctx);
-// //
-// // 							// Wrap in Intl, if intl plugin is installed
-// // 	if (ctx.Plugins.get('intl')) {
-// // 		return ctx.Plugins.get('intl').withIntl(result);
-// // 	}
-// // 	return result;
-// // }
-// //         );
-// 	}
 }
 
 export default MaterialUiPlugin;
