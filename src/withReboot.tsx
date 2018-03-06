7aef97499c67db294f0cf7d102945a9e69b1e41a
amnajunaid <amna.junaid@blueeast.com>
2018-03-06 17:08:16 +0500

    fix(withReboot.tsx): reboot children passed.

--- src/withReboot.tsx
+++ src/withReboot.tsx
@@ -3,7 +3,7 @@ import Reboot from 'material-ui/Reboot';
 import { ComponentRegistryHocItem } from '@blueeast/bluerain-os';
 
 const withReboot : ComponentRegistryHocItem = (Component) => (props) => (
-	<Reboot><Component {...props} /></Reboot>
+	<Reboot>{props.children}</Reboot>
 );
 
-export default withReboot;
+export { withReboot };
