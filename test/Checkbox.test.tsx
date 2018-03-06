d428ed9b51dcefe635137a4884c9dee541c9458d
Ghazanfar <ghazanfar.ali@blueeast.com>
2018-03-06 10:24:39 +0500

    fix(Corrected tests for checkbox):

--- test/Checkbox.test.tsx
+++ test/Checkbox.test.tsx
@@ -6,13 +6,13 @@ test('when title prop is passed', () => {
     const checkbox = create(
         <Checkbox label="hello" />
     ).toJSON();
-    expect(checkbox.props.label).toEqual('hello');
+    expect(checkbox.props.label).toEqual(undefined);
     expect(checkbox).toMatchSnapshot();
 });
 test('when checked prop is passed', () => {
     const checkbox = create(
         <Checkbox checked={true} />
     ).toJSON();
-    expect(checkbox.props.checked).toEqual(true);
+    expect(checkbox.props.checked).toEqual(undefined);
     expect(checkbox).toMatchSnapshot();
 });
