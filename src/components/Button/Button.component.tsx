7d05b41d72f943108ef92eef6ff5a12a97ade040
Muhammad Aqeel <m.aqeel@blueeast.com>
2018-09-12 12:50:13 +0500

    fix: fab

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ButtonProperties } from '@blueeast/bluerain-ui-interfaces';
 import Button from 'material-ui/Button';
 
+
 export const BlueRainButton : React.StatelessComponent<ButtonProperties> = (props) => {
 	const widthFull = { ...props.style, width:'100%' };
 	if(props.iconLeft){
@@ -12,7 +13,7 @@ export const BlueRainButton : React.StatelessComponent<ButtonProperties> = (prop
             style={props.fullWidth? widthFull: props.style}
             disabled={props.disabled}
             href={props.href}
-            fab={props.fab}
+            fab={props.rounded}
             mini={props.small}
             onClick={props.onPress}
             dense={props.small}
