6666e95976fd860e130238206718a0649bcd1074
adnan1naeem <adnan.naeem@blueeast.com>
2018-10-18 10:30:37 +0500

    fix:updated testcases of plugin

--- src/components/Modal/test/Modal.stories.tsx
+++ src/components/Modal/test/Modal.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import ModalStoryComponent from './ModalStory';
+
 const stories = storiesOf('Modal', module);
 
 stories.add('ModalComponent full width ', () => {
