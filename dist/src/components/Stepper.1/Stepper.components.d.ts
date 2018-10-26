bb22db6344e215311ee182cb90712e23eb10858c
adnan1naeem <adnan.naeem@blueeast.com>
2018-10-26 11:29:38 +0500

    fix:fied issues of Button component

--- dist/src/components/Stepper.1/Stepper.components.d.ts
+++
@@ -1,9 +0,0 @@
-import { StepperProperties } from '@blueeast/bluerain-ui-interfaces';
-import React from 'react';
-export interface StepperProps extends StepperProperties {
-    variant: 'text' | 'dots' | 'progress';
-    backButton: any;
-    nextButton: any;
-    position: 'bottom' | 'top' | 'static';
-}
-export declare const Stepper: React.StatelessComponent<any>;
