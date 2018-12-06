0481afe13f7dda5804860d5ba6d53a98325b0fde
adnan1naeem <adnan.naeem@blueeast.com>
2018-12-06 16:47:13 +0500

    fix:fixed button compoennt

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -1,38 +1,74 @@
 import * as React from 'react';
 import { ButtonProperties } from '@blueeast/bluerain-ui-interfaces';
 import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
 
 export interface MuiButtonProperties extends ButtonProperties {
 
-  size?: 'small' | 'medium' | 'large';
-  color?: any
+    size?: 'small' | 'medium' | 'large';
+    color?: any,
+    fab?: boolean
 }
 
+
 export const BlueRainButton: React.StatelessComponent<MuiButtonProperties> = (props) => {
-  const { iconLeft, onPress, transparent, ...others } = props;
-  return (
-    <Button
-      {...others}
-      color={props.primary ? 'primary' : props.color}
-      style={props.success
-        ?
-        { backgroundColor: 'green', ...props.style } : props.dark
-          ?
-          { backgroundColor: 'black', ...props.style } : props.warning
-            ?
-            { backgroundColor: 'yellow', ...props.style } : props.danger
-              ?
-              { backgroundColor: 'red', ...props.style } : props.style}
-      disabled={props.disabled}
-      href={props.href}
-      mini={props.small}
-      onClick={onPress}
-      fullWidth={props.fullWidth}
-      variant={props.rounded ? 'fab' : props.bordered ? 'outlined' : props.primary || props.light ? 'contained' : props.variant}
-      size={props.small ? 'small' : props.large ? 'large' : props.size}
-    >
-      {iconLeft ? iconLeft : null} {props.title ? props.title : null} {props.children}{props.iconRight ? props.iconRight : null}
-    </Button>
-  );
+    const { fab, iconLeft, onPress, transparent, ...others } = props;
+
+
+
+    return (
+        <>
+            {fab
+                ?
+                <Fab
+                    {...others}
+                    color={props.primary ? 'primary' : props.color}
+                    style={props.success
+                        ?
+                        { backgroundColor: 'green', ...props.style } : props.dark
+                            ?
+                            { backgroundColor: 'black', ...props.style } : props.warning
+                                ?
+                                { backgroundColor: 'yellow', ...props.style } : props.danger
+                                    ?
+                                    { backgroundColor: 'red', ...props.style } : props.style}
+                    disabled={props.disabled}
+                    href={props.href}
+                    mini={props.small}
+                    onClick={onPress}
+                    fullWidth={props.fullWidth}
+                    variant="extended"
+                    size={props.small ? 'small' : props.large ? 'large' : props.size}>
+
+                    {iconLeft ? iconLeft : null} {props.title ? props.title : null} {props.children}{props.iconRight ? props.iconRight : null}
+
+                </Fab>
+                :
+
+                <Button
+                    {...others}
+                    color={props.primary ? 'primary' : props.color}
+                    style={props.success
+                        ?
+                        { backgroundColor: 'green', ...props.style } : props.dark
+                            ?
+                            { backgroundColor: 'black', ...props.style } : props.warning
+                                ?
+                                { backgroundColor: 'yellow', ...props.style } : props.danger
+                                    ?
+                                    { backgroundColor: 'red', ...props.style } : props.style}
+                    disabled={props.disabled}
+                    href={props.href}
+                    mini={props.small}
+                    onClick={onPress}
+                    fullWidth={props.fullWidth}
+                    variant={props.rounded ? 'fab' : props.bordered ? 'outlined' : props.primary || props.light ? 'contained' : props.variant}
+                    size={props.small ? 'small' : props.large ? 'large' : props.size}
+                >
+                    {iconLeft ? iconLeft : null} {props.title ? props.title : null} {props.children}{props.iconRight ? props.iconRight : null}
+                </Button>
+            }
+        </>
+    );
 };
 
