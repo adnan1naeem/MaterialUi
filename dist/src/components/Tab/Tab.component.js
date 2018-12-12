4f13a908cafd29ac92dc9a4a897cd361083683b4
Muhammad Aqeel <m.aqeel@blueeast.com>
2018-12-12 17:11:58 +0500

    feature:tabs

---
+++ dist/src/components/Tab/Tab.component.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __importStar(require("react"));
+const Tab_1 = __importDefault(require("@material-ui/core/Tab"));
+const BluerainTab = (props) => {
+    return (React.createElement(Tab_1.default, Object.assign({}, props)));
+};
+exports.default = BluerainTab;
+//# sourceMappingURL=Tab.component.js.map
