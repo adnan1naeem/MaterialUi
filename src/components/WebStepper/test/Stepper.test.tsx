55d7ec724ad6537d4975516937b12e8296b67cb7
Muhammad Aqeel <m.aqeel@blueeast.com>
2018-12-12 17:35:59 +0500

    fix:stepper test

--- src/components/WebStepper/test/Stepper.test.tsx
+++ src/components/WebStepper/test/Stepper.test.tsx
@@ -1,4 +1,4 @@
-import { WebStepper } from '../Stepper.components';
+import { WebStepper } from '../';
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
