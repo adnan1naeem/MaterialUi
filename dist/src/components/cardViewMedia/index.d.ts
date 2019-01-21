16cacd2d42949936ef9aa63d1a3aa9585bfbdb62
ALI ZAIN JAVED <ali.javed@blueeast.com>
2019-01-21 17:14:04 +0500

    'fix'

--- dist/src/components/cardViewMedia/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as CardViewMedia, CardviewMediaProperties } from './CardViewMedia.component';
--- dist/src/components/cardViewMedia/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CardViewMedia_component_1 = require("./CardViewMedia.component");
-exports.CardViewMedia = CardViewMedia_component_1.default;
-//# sourceMappingURL=index.js.map
